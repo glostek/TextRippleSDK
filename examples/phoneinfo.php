ba92818d734bcdc2a10971fd57ea8a0ebb86240e
kgloster <kgloster@riscnetworks.com>
2015-08-07 00:38:06 -0400

    Examples 2015-08-07

---
+++ examples/phoneinfo.php
@@ -0,0 +1,32 @@
+<?php
+    
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //  phoneinfo
+    //
+    //  * * * * * * *
+    //
+    //  Retrieve demographic information from a single phone numbrer
+    //
+    //  * * * * * * *
+    //
+    //  @param  String  phn  (required)
+    //  @param  String  key  (optional) - Required if not specified in class or constructor
+    //
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    // Include Text Ripple class
+    require_once("../src/TextRipple.php");
+
+
+    // Instantiate Text Ripple
+    $tr = new TextRipple();
+
+
+    // Perform optin action and echo json response
+    $phoneinfo = $tr->phoneinfo('8885551212');
+    echo $phoneinfo;
+
+    
+?>
